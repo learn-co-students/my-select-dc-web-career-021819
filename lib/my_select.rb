a27fc5348a91cb5f2c1a708343d1b5a1a29b86e4
Mickey <micksheridan.24@gmail.com>
2019-01-29 14:23:04 -0500

    submit

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
  # your code here!
+
+ c = 0
+ ret = []
+ while c < collection.size
+    if yield(collection[c])
+
+        ret.push (collection[c])
+    end
+
+    c+= 1
+ end
+ ret
 end
