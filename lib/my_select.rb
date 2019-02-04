dd7a321a47b4cca0424390e4cff6af9e55ef1b30
mmorales4050 <mmorales4050@gmail.com>
2019-02-03 22:51:43 -0500

    My Select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  counter = 0
+  new_collection = []
+  while counter < collection.length do
+    block_given = yield(collection[counter])
+    if block_given == true
+      new_collection << collection[counter]
+      counter +=1
+    else
+      counter +=1
+    end
+  end
+  return new_collection
 end
